8bb9501db21cb848c28d4e2e09e31411156bb5ac
greenmxz <>
2017-11-04 13:08:51 -0500

    Logueo desde base de datos correcto

--- Sistema_Ventas/src/Vista/principal.java
+++ Sistema_Ventas/src/Vista/principal.java
@@ -13,6 +13,7 @@ public class principal {
      /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Login login = new Login();
